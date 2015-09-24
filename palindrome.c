043873a245b01f50157f9db94f519a8f5b22bd32
julianweisbord <julianweisbord@gmail.com>
2015-09-23 23:53:16 -0700

    Init
    Signed-off-by: julianweisbord <julianweisbord@gmail.com>

---
+++ palindrome.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "string.h"
+
+int main(int argc, char **argv){
+
+  int input;
+  for(input = 0; input <strlen(argv[1]); ++input){
+
+  }
+  printf("Length is %d\n", input);
+  int x,check = 0;
+  int i =1;
+  while(1 >0){
+    if(argv[1][x] ==argv[1][input -i]){
+      printf("check %d verified\n", x+1);
+      ++check;
+      if(check>=input/2){
+        printf("input/2: %d"input/2)
+        printf("Palindrome");
+        break;
+      }
+    }
+    else{
+      printf("Not palindrome!");
+      break;
+    }
+    ++i,++x;
+  }
+
+}
